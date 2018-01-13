1643f8f50f145a46ffb4af380f5de2a4bb5ba4de
root <root@localhost.localdomain>
2018-01-14 00:53:45 +0800

    finished del crash svg

--- app/Http/Controllers/QrcodesController.php
+++ app/Http/Controllers/QrcodesController.php
@@ -6,27 +6,29 @@ use Illuminate\Http\Request;
 use App\Book;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
 use Chumper\Zipper\Zipper;
-use File;
+use Storage;
 
 class QrcodesController extends Controller
 {
 	public function create(Request $request,Zipper $zipper)
 	{
-		// $id=intval($request->id);
-		// $book=Book::find($id);
-		// $menu=json_decode($book->menu);
-		// foreach ($menu as $value)
-		// {
-		// 	for ($i=1; $i < count($value); $i++)
-		// 	{
-		// 		$section=$value[$i];
-		// 		$sectionname=$value[$i][0];
-		// 		$sectionresourcename=$value[$i][1];
-		// 		$sectionresourcepath=$value[$i][2];
-		// 		QrCode::size(200)->generate($sectionresourcepath, public_path('storage/qrcodes/'.$sectionname.'.svg'));
-		// 	}
-		// }
-		// $zipper->make(public_path().'/storage/download/'.$book->isbn.'.zip')->folder($book->isbn)->add(public_path().'/storage/qrcodes/')->close();
-		File::delete(public_path().'/storage/qrcodes/*');
+		$id=intval($request->id);
+		$book=Book::find($id);
+		$menu=json_decode($book->menu);
+		$filelist=[];
+		foreach ($menu as $value)
+		{
+			for ($i=1; $i < count($value); $i++)
+			{
+				$section=$value[$i];
+				$sectionname=$value[$i][0];
+				$sectionresourcename=$value[$i][1];
+				$sectionresourcepath=$value[$i][2];
+				QrCode::size(200)->generate($sectionresourcepath, public_path('storage/qrcodes/'.$sectionname.'.svg'));
+				$filelist[]=$sectionname.'.svg';
+			}
+		}
+		$zipper->make(public_path().'/storage/download/'.$book->isbn.'.zip')->folder($book->isbn)->add(public_path().'/storage/qrcodes/')->close();
+		Storage::disk('qrcodes')->delete($filelist);
 	}
 }
